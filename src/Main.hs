aeb86ebc761363ef4c188610f7e36d74f8ece115
utsho <utshodebravestone@gmail.com>
2023-09-11 12:47:18 +0600

    added types to functions

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,30 @@
-main = putStrLn (makeHtml "Learning Haskell" "Hello, Haskell!")
+main :: IO()
+main = putStrLn myHtml
 
+myHtml :: String
+myHtml = makeHtml "Learning Haskell" (h1_ "Hello, Haskell!" <> p_ "I'm trying to learn you.")
+
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
-html_ content = "<html>" <> content <> "</html>"
+html_ :: String -> String
+html_ = el "html"
 
-head_ content = "<head>" <> content <> "</head>"
+head_ :: String -> String
+head_ = el "head"
 
-title_ title = "<title>" <> title <> "</title>"
+title_ :: String -> String
+title_ = el "title"
 
-body_ content = "<body>" <> content <> "</body>"
+body_ :: String -> String
+body_ = el "body"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content =
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
